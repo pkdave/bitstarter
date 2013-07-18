e0a48fb3865763f5ca2e666d7f34d6ca69846908
Punita Dave <pkdave@gmail.com>
2013-07-18 16:12:00 +0000

    Added readFileSync to web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var file1 = require('fs');
+var buf = new buffer(256);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file1.readFileSync(index.html));
 });
 
 var port = process.env.PORT || 5000;
