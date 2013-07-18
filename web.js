cc2b0483fd37b3fe84cc298bdc27c936a1cf41a2
Punita Dave <pkdave@gmail.com>
2013-07-18 17:23:40 +0000

    reading from file

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var file1 = require('fs');
-var buf = new buffer(256);
+var buf = new Buffer(256);
 file1.readFileSync("index.html", buf);
 
 app.get('/', function(request, response) {
